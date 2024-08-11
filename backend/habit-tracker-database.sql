fdbbe967e08f2d4a5a9b479bb3dfb966732bc445
SP208 <patels208208@gmail.com>
2024-08-11 13:42:20 +0100

    Backend files updated

--- backend/habit-tracker-database.sql
+++ backend/habit-tracker-database.sql
@@ -12,5 +12,11 @@ CREATE TABLE nutrition (
     completed DATETIME
 );
 
+CREATE TABLE movement (
+	habit_ID INT,
+    completed DATETIME
+);
+
 SELECT * FROM hydration;
 SELECT * FROM nutrition;
+SELECT * FROM movement;
--- backend/index.js
+++ backend/index.js
@@ -57,3 +57,17 @@ pool.query(sql, [habit_id, completed], (err, result) => {
     res.status(201).json({ message: 'Nutrition data saved successfully'});
 });
 });
+
+app.post('/movement', (req, res) => {
+    const { habit_id, completed } = req.body;
+
+const sql = 'INSERT INTO movement (habit_id, completed) VALUES (?, ?)';
+pool.query(sql, [habit_id, completed], (err, result) => {
+    if (err) {
+        console.error('Error entering movement data', err.message);
+        return res.status(500).json({ error: 'Database error' });
+    }
+
+    res.status(201).json({ message: 'Movement data saved successfully'});
+});
+});
