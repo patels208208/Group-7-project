3f1c69c194ef5efcf8b0703fe8a6761f342714ff
Hannah McIntosh <hannahjmcintosh@gmail.com>
2024-08-12 22:17:19 +0100

    basic login and register page

--- habit-tracker-app/src/App.jsx
+++ habit-tracker-app/src/App.jsx
@@ -7,15 +7,28 @@ import ReadingApp from './reading/readingApp';
 import SleepApp from './sleep/sleepApp';
 import MedicationApp from './medication/medicationApp';
 import HobbiesApp from './hobbies/hobbiesApp';
+import {Login} from './login/login.jsx';
+import {Register} from './login/register.jsx';
+import { useState } from "react";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('Login');
+  const changeForm = (formName) =>
+    setCurrentForm(formName);
+
   return (
     <div>
     <h1>Habit tracker</h1>
     <div className="App">
+    <div>
+      {
+      currentForm === 'Login' ? <Login onFormSwitch={changeForm}/> : <Register onFormSwitch={changeForm} />
+      }
+      </div>
       <Grid /> 
     </div>
     <div className="habit-tracker">
+	);
       <HydrationApp />
       <NutritionApp />
       <MovementApp />
@@ -25,6 +38,7 @@ function App() {
       <HobbiesApp />
     </div>
     </div>
+    
   );
 };
 
