758bce5007f3a78c42bc46a4b5f63854e700534b
Hannah McIntosh <hannahjmcintosh@gmail.com>
2024-08-13 21:23:29 +0100

    adding tailwind file

--- habit-tracker-app/src/login/login.jsx
+++ habit-tracker-app/src/login/login.jsx
@@ -1,28 +1,48 @@
-import React, { useState } from 'react';
-import './login.css'
+import React, { useState } from "react";
+import "./login.css";
 
 export const Login = (props) => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-    const clickSubmit = (e) => {
-        e.preventDefault(); 
-        console.log (email);
-    }
+	const clickSubmit = (e) => {
+		e.preventDefault();
+		console.log(email);
+	};
 
 	return (
-        <div className="form-container">
-             <h2>Login</h2>
-            <form className="loginForm" onSubmit={clickSubmit}>
-                <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@email.com" id="email" name="email"/>
+		<div className="form-container">
+			<h2>Login</h2>
+			<form className="loginForm" onSubmit={clickSubmit}>
+				<label htmlFor="email">Email</label>
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="youremail@email.com"
+					id="email"
+					name="email"
+				/>
 
-			    <label htmlFor="password">Password</label>
-			    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
-			   
-                <button type="submit">Log In</button>
-		    </form>
-            <button className="switchButton" onClick={() => props.onFormSwitch("Register")}> Don't have an account? Sign Up Here!</button>
-            </div>
+				<label htmlFor="password">Password</label>
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="*********"
+					id="password"
+					name="password"
+				/>
+
+				<button type="submit">Log In</button>
+			</form>
+			<button
+				className="switchButton"
+				onClick={() => props.onFormSwitch("Register")}
+			>
+				{" "}
+				Don't have an account? Sign Up Here!
+			</button>
+		</div>
 	);
 };
