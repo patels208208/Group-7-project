9c1e76f195c38d75fbeeea3bc8aabc64b4dac8a0
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-16 19:54:53 +0100

    removed previous commented-out code

--- habit-tracker-app/src/components/BottomNavigation.jsx
+++ habit-tracker-app/src/components/BottomNavigation.jsx
@@ -21,42 +21,6 @@ const BottomNavigation = () => {
         </Link>
       </div>
     </div>
-
-// {/* <>
-//   {/* component */}
-//   <div className="">
-//     <section id="bottom-navigation" class="md:hidden block fixed inset-x-0 bottom-0 z-10 bg-white shadow"> {/*// if shown only tablet/mobile*/}
-//     <section
-//       id="bottom-navigation"
-//       className="block fixed inset-x-0 bottom-0 z-10 bg-briny-500 shadow"
-//     >
-//       <div id="tabs" className="flex justify-between">
-//         <Link
-//           to="/home"
-//           className="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"
-//         >
-//           <i className="fa-solid fa-house" id="nav-icon"/>
-//           <span className="tab tab-home block text-xs text-white">Home</span>
-//         </Link>
-//         <Link
-//           to="habit-tracker"
-//           className="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"
-//         >
-//           <i className="fa-solid fa-chart-simple" id="nav-icon" />
-//           <span className="tab tab-kategori block text-xs text-white">Habits</span>
-//         </Link>
-//         <Link
-//           to="/profile"
-//           className="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1"
-//         >
-//           <i className="fa-solid fa-user" id="nav-icon" />
-//           <span className="tab tab-explore block text-xs text-white">Profile</span>
-//         </Link>
-//       </div>
-//     </section>
-//     </section>
-//   </div>
-// </> */}
   );
 };
 
