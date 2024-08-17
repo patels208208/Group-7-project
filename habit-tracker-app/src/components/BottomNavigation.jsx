e787f993bc8886c960f6d4acb304a6372ca9dc73
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-17 23:35:37 +0100

    very slight styling edits

--- habit-tracker-app/src/components/BottomNavigation.jsx
+++ habit-tracker-app/src/components/BottomNavigation.jsx
@@ -7,15 +7,15 @@ const BottomNavigation = () => {
     return (
       <div className="fixed bottom-0 left-0 right-0 bg-briny-500 shadow-lg ">
       <div className="flex justify-around items-center h-14">
-        <Link to="/home" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700">
+        <Link to="/home" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700 active:bg-briny-300 ease-in-out duration-200">
         <i className="fa-solid fa-house" id="nav-icon"/>
           <span className="text-xs mt-0.5">Home</span>
         </Link>
-        <Link to="/habit-tracker" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700">
+        <Link to="/habit-tracker" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700 active:bg-briny-300 ease-in-out duration-200">
         <i className="fa-solid fa-chart-simple" id="nav-icon" />
           <span className="text-xs mt-0.5">Habits</span>
         </Link>
-        <Link to="/profile" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700">
+        <Link to="/profile" className="flex flex-col items-center rounded-md px-5 py-1 text-white font-medium hover:bg-briny-700 active:bg-briny-300 ease-in-out duration-200">
         <i className="fa-solid fa-user" id="nav-icon" />
           <span className="text-xs mt-0.5">Profile</span>
         </Link>
--- habit-tracker-app/src/components/HabitApp.jsx
+++ habit-tracker-app/src/components/HabitApp.jsx
@@ -24,7 +24,7 @@ function HabitApp({ icon, habitName, url, habitId }) {
       <div className="flex flex-col w-72 h-32 bg-white text-melon-500 shadow-xl hover:text-briny-500 active:text-briny-300 align-middle justify-center">
         <button
           onClick={(event) => handleClick(event, url, habitId)}
-          className="text-melon-500 w-auto h-auto hover:text-briny-500 active:text-briny-300"
+          className="text-melon-500 w-auto h-auto ease-in-out duration-200 hover:text-briny-500 active:text-briny-300"
         >
           <HabitSquare icon={icon} habitName={habitName} className="text-l font-semibold font-heading my-1" />
           <p className="text-sm">Tap here when daily goal is complete</p>
