19a214953b220f017e92edeef7828981c60ef149
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-18 17:37:17 +0100

    creating habit view page

---
+++ habit-tracker-app/src/components/ViewMonthly.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MonthlyView() {
+    return(
+        <div className="flex flex-col items-center">
+            <p>Monthly View</p>
+        </div>
+    );
+};
+
+export default MonthlyView;
