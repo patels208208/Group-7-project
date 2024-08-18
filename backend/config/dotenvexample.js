7072cd02ee090c316ff275fd4bdeab739c802482
Hannah McIntosh <hannahjmcintosh@gmail.com>
2024-08-18 20:11:10 +0100

    including dotenvexamplefile

---
+++ backend/config/dotenvexample.js
@@ -0,0 +1,5 @@
+// DB_HOST ="localhost" //add in your own db localhost
+// DB_USER="root" //keep as is
+// DB_PASSWORD="password" //your own db password
+// DB_NAME="habit_tracker" //keep as is
+// PORT = "3002" //keep as is
