d9469efcb47fc005da42d8db3d3a6dca4805a026
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-18 18:06:16 +0100

    minor styling changes

--- habit-tracker-app/src/pages/HabitViews.jsx
+++ habit-tracker-app/src/pages/HabitViews.jsx
@@ -27,7 +27,7 @@ function HabitViews() {
     return(
         <div className="flex flex-col items-center mt-3">
             <div className="mt-4">
-                <select className="form-select" value={view} onChange={handleOnChange}>
+                <select className="form-select text-l p-1 rounded-md border-none shadow-md bg-ghostWhite-300 ring-1 ring-inset ring-melon-500 focus:outline-none focus:ring-inset focus:ring-2 focus:ring-melon-300" value={view} onChange={handleOnChange}>
                     <option value="selectView">Select your view</option>
                     <option value="daily">Daily</option>
                     <option value="weekly">Weekly</option>
