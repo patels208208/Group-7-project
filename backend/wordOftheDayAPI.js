8c8284a449156b8ceb18b4967ac06e82ad9adf93
Danielle Stanley <danielle@undertherowantrees.co.uk>
2024-08-19 08:50:55 +0100

    api key added in backend and dotenv file created

---
+++ backend/wordOftheDayAPI.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+const app = express();
+require('dotenv').config({ path: './.env' });
+
+app.use(cors());
+
+app.get('/api/wordOfTheDay', async (req, res) => {
+  try {
+    const response = await fetch(`https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${process.env.WORDNIK_API_KEY}`);
+    
+    
+    //Check if the response is JSON
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Invalid content-type. Expected application/json but received ' + contentType);
+    }
+    
+    const result = await response.json();
+    res.json(result);
+
+  } catch (error) {
+    console.error('Error fetching the word of the day:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(3002, () => {
+  console.log('Server running on port 3002');
+});
