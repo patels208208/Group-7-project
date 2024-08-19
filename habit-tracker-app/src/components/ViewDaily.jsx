8943cd47941b5959a46329f12a9d731a8d75168c
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-19 16:35:37 +0100

    basic weekly view table component & weekly view component

--- habit-tracker-app/src/components/ViewDaily.jsx
+++ habit-tracker-app/src/components/ViewDaily.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import HabitTable from "./HabitTable";
 
 function DailyView() {
-    return(
-        <div className="flex flex-col items-center">
-            <p>Daily View</p>
-        </div>
-    );
-};
+  return (
+    <div className="flex flex-col items-center">
+      <p>Daily View</p>
+      <HabitTable />
+    </div>
+  );
+}
 
 export default DailyView;
