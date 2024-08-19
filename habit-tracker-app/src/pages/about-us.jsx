b0175810426d346b97cb2872d85ffde958a5b5ee
SP208 <patels208208@gmail.com>
2024-08-19 22:02:27 +0100

    About page, calendarviewsApp.jsx updated

--- habit-tracker-app/src/pages/about-us.jsx
+++ habit-tracker-app/src/pages/about-us.jsx
@@ -3,7 +3,14 @@ import React from "react";
 function AboutUs() {
     return(
         <div className="about-container">
-            <p>About Page</p>
+            <h1>About Page</h1>
+            <br></br>
+            <p>We have created this habit tracking app for anyone who needs a helping hand in regularly completing tasks, goals or self-care.
+                Whether you're turning over a new leaf, making a fresh start or trying to stick to your New Year's resolutions - this app will be
+                of help!
+                We wanted to cater to neurodivergent users in particular, and have kept this in mind when designing and creating the app.</p>
+                <br></br>
+            <p>This app has been created by: Danielle, Hannah, Kat, Meg and Sabeeha.</p>
         </div>
     );
 };
