356a4f814d2c86bc26f986b50e415285247a9236
Danielle Stanley <danielle@undertherowantrees.co.uk>
2024-08-20 17:22:43 +0100

    Goal confirmation dispaying on goal setting page but no icon

--- habit-tracker-app/src/components/MeasurementInput.jsx
+++ habit-tracker-app/src/components/MeasurementInput.jsx
@@ -1,9 +1,12 @@
-const MeasurementInput = () => {
+const MeasurementInput = ({setSelectedQuantity}) => {
+  const handleChange = (event) => {
+    setSelectedQuantity(event.target.value);
+  };
 
   return (
     <div className="flex flex-col items-center">
       <h3 className="text-silverMedal-900"></h3>
-      <input placeholder="1" className="w-100 gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-silverMedal-800 shadow-sm ring-1 ring-inset ring-silverMedal-500 hover:bg-briny-100" type="number" />
+      <input onChange={handleChange} name="measurement-input" placeholder="Quantity" className="w-50 gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-silverMedal-800 shadow-sm ring-1 ring-inset ring-silverMedal-500 hover:bg-briny-100" type="number" />
     </div>
   )
 }
--- habit-tracker-app/src/pages/GoalSetting.jsx
+++ habit-tracker-app/src/pages/GoalSetting.jsx
@@ -8,40 +8,57 @@ import MeasurementDropdown from '../components/MeasurementDropdown';
 import MeasurementInput from '../components/MeasurementInput';
 
 const GoalSetting = () => {
+  const [selectedQuantity, setSelectedQuantity] = useState('');
   const [selectedMeasurement, setSelectedMeasurement] = useState('');
   const [selectedHabitSquare, setSelectedHabitSquare] = useState('');
   const [selectedFrequency, setSelectedFrequency] = useState('');
   const [selectedGoal, setSelectedGoal] = useState('');
+
+  const handleSelectFrequency = (frequency) => {
+    setSelectedFrequency(frequency);
+    console.log(frequency);
+  }
+
+  const handleSelectMeasurement = (measurement) => {
+    setSelectedMeasurement(measurement);
+    console.log(measurement);
+  }
+
   return (
     <div className='flex justify-center width-auto'>
       <div>
         <div>
-          <div class="flex flex-col items-center">
+          <div className="flex flex-col items-center">
             <h1 className="text-silverMedal-900">Goal Setting</h1>
             <h3 className="text-silverMedal-900">Select the habit you wish to track:</h3>
           </div>
           <Grid selectedHabitSquare={selectedHabitSquare} setSelectedHabitSquare={setSelectedHabitSquare}/>
-          <div class="flex flex-col items-center">
+          <div className="flex flex-col items-center">
+          <h3>How would you like to track this habit?</h3>
             <div className="flex">
-            <MeasurementInput />
-            <MeasurementDropdown selectedMeasurement={selectedMeasurement} setSelectedMeasurement={setSelectedMeasurement}/>
+            <MeasurementInput selectedQuantity={selectedQuantity} setSelectedQuantity={setSelectedQuantity}/>
+            <MeasurementDropdown handleSelectMeasurement={handleSelectMeasurement} selectedMeasurement={selectedMeasurement} setSelectedMeasurement={setSelectedMeasurement}/>
             </div>
             <div className="flex">
             <GoalInput selectedGoal={selectedGoal} setSelectedGoal={setSelectedGoal}/>
-            <FrequencyDropdown selectedFrequency={selectedFrequency} setSelectedFrequency={setSelectedFrequency}/>
+            <FrequencyDropdown handleSelectFrequency={handleSelectFrequency} selectedFrequency={selectedFrequency} setSelectedFrequency={setSelectedFrequency}/>
             </div>
           </div>
         </div>
         <div className='flex flex-col items-center space-y-2 mt-2'>
           <GoalSetButton 
-          setSelectedHabitSquare={setSelectedHabitSquare}
-          setSlectedFrequency={setSelectedFrequency}
-          setSelectedGoal={setSelectedGoal}
+          selectedHabitSquare={selectedHabitSquare}
+          selectedQuantity={selectedQuantity}
+          selectedMeasurement={selectedMeasurement}
+          selectedFrequency={selectedFrequency}
+          selectedGoal={selectedGoal}
           />
           <NewGoalButton 
-          setSelectedHabitSquare={setSelectedHabitSquare}
-          setSelectedFrequency={setSelectedFrequency}
-          setSelectedGoal={setSelectedGoal}/>
+          selectedHabitSquare={selectedHabitSquare}
+          selectedQuantity={selectedQuantity}
+          selectedMeasurement={selectedMeasurement}
+          selectedFrequency={selectedFrequency}
+          selectedGoal={selectedGoal}/>
         </div>
       </div>
   </div>
