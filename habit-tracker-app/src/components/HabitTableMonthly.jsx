80a8062396385c177f05877f912ee261fb468b5d
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-21 22:42:08 +0100

    adding a table component for monthly habit view

---
+++ habit-tracker-app/src/components/HabitTableMonthly.jsx
@@ -0,0 +1,13 @@
+import { faStar, faSquareCheck, faSquareXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
+
+const HabitTableMonthly = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HabitTableMonthly;
