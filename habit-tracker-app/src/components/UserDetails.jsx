4a5116694aa4ff79c140226a1996f66085e6d390
Danielle Stanley <danielle@undertherowantrees.co.uk>
2024-08-22 15:27:16 +0100

    user details fetched from DB

--- habit-tracker-app/src/components/UserDetails.jsx
+++ habit-tracker-app/src/components/UserDetails.jsx
@@ -1,16 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const UserDetails = ({ user }) => {
-  const firstName = user.firstName;
-  const surname = user.surname;
-  const email = user.email;
- 
+
+const UserDetails = ({ userId }) => {
+  const [user, setUser] = useState({ firstName: "", surname: "", email: "" });
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await axios.get(`/api/users/${userId}`);
+        const userData = response.data;
+        setUser({
+          firstName: userData.first_name,
+          surname: userData.surname,
+          email: userData.email_address,
+      });
+      } catch (error) {
+        console.error("Error fetching user details: ", error.message);
+      }
+    };
+    fetchUserDetails();
+  }, [userId]);
 
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl font-bold text-center text-briny-600">
-        {/* {firstName} {surname} */}
+        {user.firstName} {user.surname}
       </h2>
-      {/* <p className="text-lg text-center text-briny-600">{email}</p> */}
+      <p className="text-lg text-center text-briny-600">{user.email}</p>
       
     </div>
   );
