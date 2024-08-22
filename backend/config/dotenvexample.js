e6206dfd11bec6ef2e18c2910c7c86b1c0abfa92
patels208208 <patels208208@gmail.com>
2024-08-22 19:20:33 +0100

    Update dotenvexample.js

--- backend/config/dotenvexample.js
+++ backend/config/dotenvexample.js
@@ -2,4 +2,3 @@
 // DB_USER="root" //keep as is
 // DB_PASSWORD="password" //your own db password
 // DB_NAME="habit_tracker" //keep as is
-// WORDNIK_API_KEY="dvsmo4ipzfgw1v9fl39ps3i51bd30ysawqs22mt5q77pm66tw" //keep as is
