7f6112f55f90c350bb141689cd626b884ba40761
Hannah McIntosh <hannahjmcintosh@gmail.com>
2024-08-24 19:03:31 +0100

    adding current week date range

--- habit-tracker-app/src/components/UserContext.jsx
+++ habit-tracker-app/src/components/UserContext.jsx
@@ -5,29 +5,31 @@ import axios from "axios";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+	const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await axios.get("/api/user/current");
-                setUser(response.data);
-            } catch (error) {
-                console.error("Error fetching current user: ", error.message);
-            }
-        };
-        fetchUser();
-    }, []);
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await axios.get(
+					"http://localhost:3001/api/user/current"
+				);
+				setUser(response.data);
+			} catch (error) {
+				console.error("Error fetching current user: ", error.message);
+			}
+		};
+		fetchUser();
+	}, []);
 
-    const updateUser = (userData) => {
-        setUser(userData);
-    };
+	const updateUser = (userData) => {
+		setUser(userData);
+	};
 
-    return (
-        <UserContext.Provider value={{ user, updateUser }}>
-            {children}
-        </UserContext.Provider>
-    );
+	return (
+		<UserContext.Provider value={{ user, updateUser }}>
+			{children}
+		</UserContext.Provider>
+	);
 };
 
 export const useUser = () => useContext(UserContext);
--- habit-tracker-app/src/components/ViewWeekly.jsx
+++ habit-tracker-app/src/components/ViewWeekly.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import HabitTableWeekly from "./HabitTableWeekly";
+import { startOfWeek, endOfWeek, format } from "date-fns";
+
+const getCurrentWeekRange = () => {
+	const now = new Date();
+	const start = startOfWeek(now, { weekStartsOn: 1 });
+	const end = endOfWeek(now, { weekStartsOn: 1 });
+	return `${format(start, "MMM dd")} - ${format(end, "MMM dd")}`;
+};
+
+const currentWeekRange = getCurrentWeekRange();
 
 function WeeklyView() {
-    return(
-        <div className="flex flex-col items-center">
-            <p>Weekly View</p>
-            <HabitTableWeekly />
-        </div>
-    );
-};
+	return (
+		<div className="flex flex-col items-center">
+			<p>Weekly View</p>
+			<div>
+				<div className="text-center text-lg font-bold mb-4">
+					{currentWeekRange}
+				</div>
+			</div>
+			<HabitTableWeekly />
+		</div>
+	);
+}
 
 export default WeeklyView;
