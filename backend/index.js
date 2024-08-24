fd328d426bcd9126965a3d36de5165a099895f3f
SP208 <patels208208@gmail.com>
2024-08-24 16:29:57 +0100

    Minor updates to repo

--- backend/index.js
+++ backend/index.js
@@ -41,24 +41,24 @@ app.use((err, req, res, next) => {
 
 // Word of the Day API
 app.get('/api/wordOfTheDay', async (req, res) => {
-	try {
-	  const response = await fetch(`https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${process.env.WORDNIK_API_KEY}`);
-	  
-	  
-	  //Check if the response is JSON
-	  const contentType = response.headers.get('content-type');
-	  if (!contentType || !contentType.includes('application/json')) {
-		throw new Error('Invalid content-type. Expected application/json but received ' + contentType);
-	  }
-	  
-	  const result = await response.json();
-	  res.json(result);
-  
-	} catch (error) {
-	  console.error('Error fetching the word of the day:', error);
-	  res.status(500).json({ error: 'Internal server error' });
-	}
-  });
+  try {
+    const response = await fetch(`https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${process.env.WORDNIK_API_KEY}`);
+    
+    
+    //Check if the response is JSON
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Invalid content-type. Expected application/json but received ' + contentType);
+    }
+    
+    const result = await response.json();
+    res.json(result);
+
+  } catch (error) {
+    console.error('Error fetching the word of the day:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // use auth
 app.use('/api', auth);
