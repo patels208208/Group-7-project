b3ac3993aa4f5bfd887de78c30133e6e465476f1
Avo Cado <avo.cado@codefirstgirls.com>
2024-08-25 20:09:49 +0100

    Redux files cleanup

--- habit-tracker-app/src/redux/store.js
+++ habit-tracker-app/src/redux/store.js
@@ -1,11 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import rootReducer from './habitReducer'; // Your combined reducers
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "./habitReducer"; // Your combined reducers
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk),
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 window.store = store;
 export default store;
